6e9487f630329cacb1c5c3afabf00be959b19f0c
zdazx <zhangduo0227@163.com>
2020-07-19 23:02:59 +0800

    add listener for button1

--- app/src/main/java/com/thoughtworks/helloworld/MainActivity.java
+++ app/src/main/java/com/thoughtworks/helloworld/MainActivity.java
@@ -2,7 +2,10 @@ package com.thoughtworks.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button1 = findViewById(R.id.button_label1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSecondActivity();
+            }
+        });
+    }
+
+    private void openSecondActivity() {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 }
