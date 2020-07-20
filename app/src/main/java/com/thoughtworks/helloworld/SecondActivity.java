0beef4091d3b29622c12b410d874e48155b33fec
zdazx <zhangduo0227@163.com>
2020-07-20 18:18:45 +0800

    click log in button in second layout and handle it in second activity

--- app/src/main/java/com/thoughtworks/helloworld/SecondActivity.java
+++ app/src/main/java/com/thoughtworks/helloworld/SecondActivity.java
@@ -1,6 +1,9 @@
 package com.thoughtworks.helloworld;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +13,17 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Button logInBtn = findViewById(R.id.log_in);
+        logInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLogInActivity();
+            }
+        });
+    }
+
+    private void openLogInActivity() {
+        Intent intent = new Intent(this, LogInActivity.class);
+        startActivity(intent);
     }
 }
